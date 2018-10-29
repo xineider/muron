101c58313861eeb5839e6095d20e4cf7a10a1f35
Marcos <marcos.schneider13@hotmaill.com>
2018-10-29 13:43:32 -0300

    Teste heroku

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,13 +299,6 @@ $(document).on('ready', function () {
 
 	$(".sidenav-trigger").sidenav();
 
-	// $(".button-collapse").sideNav({
-	//     menuWidth: 300, // Default is 300
-	//     edge: 'right', // Choose the horizontal origin
-	//     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-	//     draggable: true // Choose whether you can drag to open on touch screens,
-	//   });
-
 	window.onpopstate = function() {
 		console.log('EEEEEEEEEEEEEE ESTOU NO ONPOPSTATE EEEEEEEEEEE');
 		console.log(location.pathname);
