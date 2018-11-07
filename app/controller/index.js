965aa898bb51ee972fe637d26844f15d081e2d3e
Marcos <marcos.schneider13@hotmaill.com>
2018-11-07 16:30:58 -0200

    Adicionado ajax-load-back

--- app/controller/index.js
+++ app/controller/index.js
@@ -74,10 +74,6 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.get('/loginfake', function(req, res, next) {
-	res.render(req.isAjaxRequest() == true ? 'api' : 'montadorLimpo', {html: 'inicio/login_fake', data: data, usuario: req.session.usuario});
-});
-
 /* POST enviando o login para verificação. */
 router.post('/', function(req, res, next) {
 	// Recebendo o valor do post
