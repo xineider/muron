e5a4700222fbb37f8bb9d1c428e4325c281e6a32
Marcos <marcos.schneider13@hotmaill.com>
2019-01-22 17:47:35 -0200

    Alterações no recuperar senha para colocar no padrao dos outros

--- app/controller/api.js
+++ app/controller/api.js
@@ -58,26 +58,27 @@ router.post('/', function(req, res, next) {
 
 router.post('/recuperar/senha', function(req, res, next) {
 	var post = req.body;
+	var data_insert;
+	var nova_senha;
 	console.log('00000000000000 É isso que recebo do recuperar senha 000000000000000');
 	console.log(post);
 	console.log('0000000000000000000000000000000000000000000000000000000000000000000');
 	model.PesquisarEmail(post.email).then(idEmail => {
 		if(idEmail != ''){
-			var nova_senha = Math.random().toString(36).substring(7);
-			var html = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
-			"<br>Sua nova senha no Muron é: "+nova_senha+
-			"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@muron.com.br'>suporte@muron.com.br</a>"+
-			'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
-			'<br>Não responda esta mensagem, ela é enviada automaticamente.';
-			var text = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
-			"<br>Sua nova senha no Muron é: "+nova_senha+
-			"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@muron.com.br'>suporte@muron.com.br</a>"+
-			'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
-			'<br>Não responda esta mensagem, ela é enviada automaticamente.';
-			
-			control.SendMail(post.email, 'Recuperação de Senha - MurOn', html, text);
-			var data_insert = {id: idEmail[0].id, senha: nova_senha};
+			nova_senha = Math.random().toString(36).substring(7);
+			data_insert = {id: idEmail[0].id, senha: nova_senha};
 			model.AlterarSenhaUsuarioPorId(data_insert).then(data_alterado_sucesso =>{
+				var html = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
+				"<br>Sua nova senha no Muron é: "+nova_senha+
+				"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@muron.com.br'>suporte@muron.com.br</a>"+
+				'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
+				'<br>Não responda esta mensagem, ela é enviada automaticamente.';
+				var text = "Olá, você está recebendo este e-mail pois pediu para recuperar sua senha"+
+				"<br>Sua nova senha no Muron é: "+nova_senha+
+				"<br>Caso não pediu para recuperar a sua senha entre em contato com o Suporte pelo e-mail <a href='mailto:suporte@muron.com.br'>suporte@muron.com.br</a>"+
+				'<br><br>Não mostre seu login e senha para ninguém. A sua conta é responsabilidade sua.'+
+				'<br>Não responda esta mensagem, ela é enviada automaticamente.';
+				control.SendMail(post.email, 'Recuperação de Senha - MurOn', html, text);				
 				res.json(data_alterado_sucesso);
 			});
 
@@ -85,9 +86,6 @@ router.post('/recuperar/senha', function(req, res, next) {
 			res.json(['email_nao_cadastrado']);
 		}
 	});
-
-	
-	// res.json(10);
 });
 
 router.post('/cadastrar/usuario', function(req, res, next) {
